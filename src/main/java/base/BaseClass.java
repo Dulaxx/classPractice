c2f8e2904efaa46b2bbc9aa2dc8735c9eea442d4
User <tharidukad@gmail.com>
2023-04-04 19:37:54 +0530

    update

---
+++ src/main/java/base/BaseClass.java
@@ -0,0 +1,36 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+    public WebDriver driver;
+    public Dotenv dotenv = Dotenv.configure().load();
+
+    @BeforeClass
+    public void setup(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(dotenv.get("TESTURL"));
+
+    }
+
+    @AfterClass
+    public void result(){
+        driver.close();
+        driver.quit();
+    }
+
+
+
+}
+
+
