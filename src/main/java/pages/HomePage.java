45caf82a50bd67d0520632e7c1ca0b8382040379
User <tharidukad@gmail.com>
2023-04-05 19:10:40 +0530

    update

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,4 +1,105 @@
 package pages;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
 public class HomePage {
+    WebDriver driver;
+    Dotenv dotenv = Dotenv.configure().load();
+    public SoftAssert sa;
+
+    By alertBoxPageTitle = By.xpath("//h1[contains(text(),'Alert Box Examples')]");
+    By showAlertBox = By.id("alertexamples");
+    By showConfirmBox = By.id("confirmexample");
+    By showConfirmBoxMessage = By.id("confirmreturn");
+    By showPromptBox = By.id("promptexample");
+    By showPromptBoxMessage = By.id("promptreturn");
+    By showPromptBoxCancelMessage = By.id("promptexplanation");
+
+    public HomePage (WebDriver driver){
+        this.driver = driver;
+    }
+    public void verifyAlertBoxPageTitle(){
+        String actualTitle = driver.findElement(alertBoxPageTitle).getText();
+        String expectTitle = "Alert Box Examples";
+        Assert.assertEquals(actualTitle,expectTitle,"Expected title is 'Alert Box Examples' ");
+        //sa.assertEquals(actualTitle,expectTitle);
+        //sa.assertAll();
+    }
+    public void clickShowAlertBox(){
+        driver.findElement(showAlertBox).click();
+    }
+    public void getAlertBoxText(){
+        driver.switchTo().alert();
+        String actualTile = driver.switchTo().alert().getText();
+        String expectedTitle = "I am an alert box!";
+        Assert.assertEquals(actualTile,expectedTitle,"Expected title is 'I am an alert box!' ");
+        //sa.assertEquals(actualTile,expectedTitle,"Expected title is 'I am an alert box!' ");
+        //sa.assertAll();
+    }
+    public void acceptAlertBox(){
+        driver.switchTo().alert().accept();
+    }
+    public void clickShowConfirmBox(){
+        driver.findElement(showConfirmBox).click();
+    }
+    public void getShowConfirmBoxText(){
+        String actualTitle = driver.switchTo().alert().getText();
+        String expectedTitle = "I am a confirm alert";
+        Assert.assertEquals(actualTitle,expectedTitle, "Expected title is 'I am a confirm alert' ");
+    }
+    public void clickOKShowConfirmBox(){
+        driver.switchTo().alert();
+        driver.switchTo().alert().accept();
+    }
+    public void verifyShowConfirmBoxOkMessage(){
+        String actualTitle = driver.findElement(showConfirmBoxMessage).getText();
+        String expectedTitle = "true";
+        Assert.assertEquals(actualTitle,expectedTitle,"Expected title is 'true' ");
+    }
+    public void clickCancelShowConfirmBox(){
+        driver.switchTo().alert().dismiss();
+    }
+    public void verifyShowConfirmBoxCancelMessage(){
+        String actualTitle = driver.findElement(showConfirmBoxMessage).getText();
+        String expectedTitle = "false";
+        Assert.assertEquals(actualTitle,expectedTitle,"Expected title is 'false' ");
+    }
+    public void clickShowPromptBox(){
+        driver.findElement(showPromptBox).click();
+    }
+    public void verifyShowPromptBox(){
+        String actualTitle = driver.switchTo().alert().getText();
+        String expectedTitle = "I prompt you";
+        Assert.assertEquals(actualTitle,expectedTitle,"Expected title is 'I prompt you' ");
+    }
+    public void clickOKShowPromptBox(){
+        driver.switchTo().alert().accept();
+    }
+    public void enterTextShowPromptBox(String msg){
+        driver.switchTo().alert().sendKeys(msg);
+    }
+    public void verifyShowPromptBoxMessage(){
+        String actualTitle = driver.findElement(showPromptBoxMessage).getText();
+        String expectedTitle = this.dotenv.get("POPUPMESSAGE");
+        Assert.assertEquals(actualTitle,expectedTitle,"Expected title is " + this.dotenv.get("POPUPMESSAGE"));
+    }
+    public void clickCancelShowPromptBox(){
+        driver.switchTo().alert().dismiss();
+    }
+    public void verifyClickCancelShowPromptBoxMessage(){
+        String actualTitle = driver.findElement(showPromptBoxCancelMessage).getText();
+        String expectedTitle = "You clicked Cancel. 'prompt' returned null";
+        Assert.assertEquals(actualTitle,expectedTitle);
+    }
+
+
+
+
+
+
+
 }
--- src/test/java/alertBox.java
+++ src/test/java/alertBox.java
@@ -1,2 +1,63 @@
-public class alertBox {
+import base.BaseClass;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class alertBox extends BaseClass {
+
+    @Test(priority = 1, description = "Validate whether an alert box displayed when user click on show alert box button")
+    public void testCase1(){
+        HomePage homePage = new HomePage(driver);
+        homePage.verifyAlertBoxPageTitle();
+        homePage.clickShowAlertBox();
+        homePage.getAlertBoxText();
+        homePage.acceptAlertBox();
+        System.out.println("Test Case ID1 - Pass");
+    }
+    @Test(priority = 2, description = "Validate whether alert message is displayed when user click on show confirm box " +
+            "and when user click OK verify whether the webpage displays confirmation message ")
+    public void testCase2(){
+        HomePage homePage = new HomePage(driver);
+        homePage.verifyAlertBoxPageTitle();
+        homePage.clickShowConfirmBox();
+        homePage.getShowConfirmBoxText();
+        homePage.clickOKShowConfirmBox();
+        homePage.verifyShowConfirmBoxOkMessage();
+        System.out.println("Test Case ID2 - Pass");
+    }
+    @Test(priority = 3, description = "Validate whether alert message is displayed when user click on show confirm box " +
+            "and when user click cancel verify whether the webpage displays confirmation message ")
+    public void testCase3(){
+        HomePage homePage = new HomePage(driver);
+        homePage.verifyAlertBoxPageTitle();
+        homePage.clickShowConfirmBox();
+        homePage.getShowConfirmBoxText();
+        homePage.clickCancelShowConfirmBox();
+        homePage.verifyShowConfirmBoxCancelMessage();
+        System.out.println("Test Case ID3 - Pass");
+    }
+    @Test(priority = 4, description = "Validate whether user entered message in the popup window is getting displayed")
+    public void testCase4(){
+        HomePage homePage = new HomePage(driver);
+        homePage.verifyAlertBoxPageTitle();
+        homePage.clickShowPromptBox();
+        homePage.verifyShowPromptBox();
+        homePage.enterTextShowPromptBox(this.dotenv.get("POPUPMESSAGE"));
+        homePage.clickOKShowPromptBox();
+        homePage.verifyShowPromptBoxMessage();
+        System.out.println("Test Case ID4 - Pass");
+        //System.out.println(this.dotenv.get("POPUPMESSAGE"));
+    }
+    @Test(priority = 5, description = "Validate whether user entered message in the popup window is getting displayed")
+    public void testCase5(){
+        HomePage homePage = new HomePage(driver);
+        homePage.verifyAlertBoxPageTitle();
+        homePage.clickShowPromptBox();
+        homePage.verifyShowPromptBox();
+        homePage.clickCancelShowPromptBox();
+        homePage.verifyClickCancelShowPromptBoxMessage();
+        System.out.println("Test Case ID5 - Pass");
+    }
+
+
+
 }
